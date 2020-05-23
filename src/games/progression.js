bac40f51c96dea8ff7c09983ca9d3b378ce7607f
Kirill Bogdanov <kiralbogdanov@gmail.com>
2020-05-23 14:58:15 +0300

    added replaceElementInProgression func

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,6 +4,7 @@ const FIRST_ITEM_LOWER_BOUNDARY = 1;
 const FIRST_ITEM_HIGHER_BOUNDARY = 100;
 const STEP_LOWER_BOUNDARY = 1;
 const STEP_HIGHER_BOUNDARY = 10;
+const PROGRESSION_ITEM_REPLACER = '..';
 
 const createProgression = (firstItem, step, length = 10) => {
   const result = [];
@@ -15,19 +16,28 @@ const createProgression = (firstItem, step, length = 10) => {
   return result;
 };
 
+const replaceElementInProgression = (progression, index, replacer) => {
+  const result = [...progression];
+
+  result[index] = replacer;
+
+  return result;
+};
+
 const createProgressionGameData = (numberOfRounds) => {
   const result = [];
 
   for (let i = 0; i < numberOfRounds; i += 1) {
     const firstItem = getRandomNumber(FIRST_ITEM_LOWER_BOUNDARY, FIRST_ITEM_HIGHER_BOUNDARY);
     const step = getRandomNumber(STEP_LOWER_BOUNDARY, STEP_HIGHER_BOUNDARY);
-    const prog = createProgression(firstItem, step);
-    const randomIndex = getRandomNumber(0, prog.length - 1);
-    const correctAnswer = prog[randomIndex];
+    const progression = createProgression(firstItem, step);
+    const randomIndex = getRandomNumber(0, progression.length - 1);
+    const correctAnswer = progression[randomIndex];
 
-    prog[randomIndex] = '..';
 
-    result.push([prog.join(' '), correctAnswer]);
+    result.push(
+      [replaceElementInProgression(progression, randomIndex, PROGRESSION_ITEM_REPLACER).join(' '), correctAnswer],
+    );
   }
 
   return result;
