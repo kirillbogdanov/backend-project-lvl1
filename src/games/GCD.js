18dc1020af07b6e3701aa3f8d8687e59d171124c
Kirill Bogdanov <kiralbogdanov@gmail.com>
2020-05-23 14:31:47 +0300

    Shortened findGCD func

--- src/games/GCD.js
+++ src/games/GCD.js
@@ -3,17 +3,12 @@ import { getRandomNumber } from '../utils/utils.js';
 const NUMBER_LOWER_BOUNDARY = 1;
 const NUMBER_HIGHER_BOUNDARY = 100;
 
-const findGcd = (a, b) => {
-  let [min, max] = [a, b].sort();
-
-  while (min !== 0) {
-    const remainder = max % min;
-
-    max = min;
-    min = remainder;
+const findGCD = (a, b) => {
+  if (b === 0) {
+    return a;
   }
 
-  return max;
+  return findGCD(b, a % b);
 };
 
 const createGCDGameData = (numberOfRounds) => {
@@ -23,7 +18,7 @@ const createGCDGameData = (numberOfRounds) => {
     const firstNum = getRandomNumber(NUMBER_LOWER_BOUNDARY, NUMBER_HIGHER_BOUNDARY);
     const secondNum = getRandomNumber(NUMBER_LOWER_BOUNDARY, NUMBER_HIGHER_BOUNDARY);
 
-    result.push([`${firstNum} ${secondNum}`, findGcd(firstNum, secondNum)]);
+    result.push([`${firstNum} ${secondNum}`, findGCD(firstNum, secondNum)]);
   }
 
   return result;
